0ab0f39a71586ede225f4ab8528fb0b324a77d2c
muzammal <muzammal@commercescience.com>
2022-11-16 11:14:45 +0500

    chat page updated.

--- src/app/chat/chat.page.ts
+++ src/app/chat/chat.page.ts
@@ -1,10 +1,10 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {AngularFireDatabase} from '@angular/fire/database';
 import {Observable} from 'rxjs';
-import { NavController, NavParams } from 'ionic-angular';
 import {AngularFirestore} from '@angular/fire/firestore';
 import {IonContent} from '@ionic/angular';
 import {ActivatedRoute} from '@angular/router';
+import {UtilsService} from "../services/utils.service";
 @Component({
     selector: 'app-chat',
     templateUrl: './chat.page.html',
@@ -23,44 +23,29 @@ messages = [];
     recivedData = [];
     // @ts-ignore
     @ViewChild(IonContent) content: IonContent;
-    constructor(public db: AngularFireDatabase,
+    constructor(private utils: UtilsService,
+                public db: AngularFireDatabase,
                 private route: ActivatedRoute,
                 private firestore: AngularFirestore) {
+        this.utils.presentLoading("Please wait...");
         this.s = this.db.list('/messages').valueChanges().subscribe( data => {
-            console.log('data', data);
+            this.utils.stopLoading();
             this.recivedData = data;
             this.messages = this.recivedData.filter(x => x.user === this.donnerName || x.user === this.currentUser);
-            console.log('messages after filter', this.messages);
         });
     }
     ngOnInit() {
         this.route.paramMap.subscribe(paramMap => {
             this.donnerName = paramMap.get('donnerName');
-            console.log('donner name: ', this.donnerName);
-            console.log('params : ', paramMap);
             this.loadUser();
-            console.log('current user:', this.currentUser);
         });
     }
 
     loadUser() {
         this.user = JSON.parse(localStorage.getItem('user'));
         this.role = localStorage.getItem('role');
-        console.log('role', this.role);
-        console.log('user', this.user);
-        // if (this.role === 'donner') {
-        //     this.currentUser = this.user.user.first_name + ' ' + this.user.user.last_name;
-        //     console.log('donner', this.currentUser);
-        // }
-        // if (this.role === 'admin') {
-        //     this.currentUser = this.user.first_name + ' ' + this.user.last_name;
-        //     console.log('admin', this.currentUser);
-        // }
-        // if (this.role === 'charity house') {
-        //     this.currentUser = this.user.user.first_name + ' ' + this.user.user.last_name;
-        //     console.log('charity house', this.currentUser);
-        // }
     }
+
     sendMessage() {
         this.db.list('/messages').push({
             user: this.currentUser,
@@ -69,18 +54,7 @@ messages = [];
         });
         this.newMsg = '';
         setTimeout(() => {
-                this.content.scrollToBottom(10);
-            });
-
-        // this.messages.push({
-        //     msg: this.newMsg,
-        //     user: 'umar',
-        //     createdAt: new Date().getTime(),
-        // });
-        // this.newMsg = '';
-        // setTimeout(() => {
-        //     this.content.scrollToBottom(200);
-        // });
+            this.content.scrollToBottom(10);
+        });
     }
-
 }
